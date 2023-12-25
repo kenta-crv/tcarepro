9beed32f17b6fa0e6a726be888e32317ef6668c4
kenta-crv <okuyamakenta@gmail.com>
2023-12-25 23:07:16 +0900

    knowledge

--- db/migrate/20231221135209_create_knowledges.rb
+++ db/migrate/20231221135209_create_knowledges.rb
@@ -1,10 +1,13 @@
 class CreateKnowledges < ActiveRecord::Migration[5.1]
   def change
     create_table :knowledges do |t|
-      t.references :contract, foreign_key: true
-      t.string :question
+      t.string :title
+      t.string :category
       t.string :genre
-      t.string :answer
+      t.string :file
+      t.string :file_2
+      t.string :priority
+      t.string :body
       t.timestamps
     end
   end
