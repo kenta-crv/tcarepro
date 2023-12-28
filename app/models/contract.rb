4d15ae6dfe5e1d839d0484e4438022ea1bb6abc0
kenta-crv <okuyamakenta@gmail.com>
2023-12-29 08:22:53 +0900

    contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,9 +1,5 @@
 class Contract < ApplicationRecord
   has_many :images
   has_many :knowledges
-  belongs_to :customer
-  has_many :calls
-
-  serialize :search, JSON
-  serialize :target, JSON
+  #has_many :calls
 end
