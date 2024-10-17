a53eb55695a3b78423081f9d15e2e8ed1b62d922
kenta-crv <okuyamakenta@gmail.com>
2024-10-17 15:10:41 +0900

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://tcare.pro:6379/0' }
+    config.redis = { url: 'redis://0.0.0.0:6379/0' }
   end
   
   Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://tcare.pro:6379/0' }
+    config.redis = { url: 'redis://0.0.0.0:6379/0' }
   end
   
