1a38b39b6ad0d3aed4460707553c7bd74fe64d62
kenta-crv <okuyamakenta@gmail.com>
2025-05-21 19:59:15 +0900

    fix

--- app/jobs/customer_import_job.rb
+++ app/jobs/customer_import_job.rb
@@ -2,10 +2,12 @@ class CustomerImportJob < ApplicationJob
   queue_as :default
 
 
-  def perform(file_path, skip_repurpose: false)
-    file = File.open(file_path)
-    Customer.all_import(file, skip_repurpose: skip_repurpose)
-    file.close
-    File.delete(file_path) if File.exist?(file_path)
-  end
+  def perform(file_path, options = {})
+  skip_repurpose = options['skip_repurpose'] == true
+
+  file = File.open(file_path)
+  Customer.all_import(file, skip_repurpose: skip_repurpose)
+  file.close
+  File.delete(file_path) if File.exist?(file_path)
+end
 end
